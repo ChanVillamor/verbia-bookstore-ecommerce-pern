cb523c965285c8be611a4af8710f35ddfdc7589a
ChanVillamor <chanmichaelespinavillamor@gmail.com>
2025-06-21 22:02:06 +0800

    update CORS at app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,7 +5,10 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: [
+    'https://verbia-bookstore-ecommerce-pern.vercel.app/',
+    'http://localhost:5173' 
+  ],
   credentials: true
 }));
 app.use(express.json());
